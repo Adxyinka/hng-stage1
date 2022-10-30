0b0c661ffb7d07011aeedfc8bfe427a5873094a8
blacczen <adeyinkaryusuf@gmail.com>
2022-10-30 19:04:05 +0100

    stage-1 task

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/api', (req, res) => {
+    res.json({"slackUsername": "Blacczen", "backend": true, "age": 16, "bio": "A graduate of history and diplomatic studies and a Node.js Backend developer"})
+})
+
+app.listen(port, () => {
+    console.log(`server running at localhost:${port}`)
+})
