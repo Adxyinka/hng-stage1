f30f01bc911302f56e3edf2146f178d71cf1f5cb
blacczen <adeyinkaryusuf@gmail.com>
2022-10-30 19:18:33 +0100

    changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.json({"slackUsername": "Blacczen", "backend": true, "age": 16, "bio": "A graduate of history and diplomatic studies and a Node.js Backend developer"})
 })
 
